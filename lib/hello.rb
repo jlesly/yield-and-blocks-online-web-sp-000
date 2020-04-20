bb2f73a8dedac84c94044476a50856013372cafa
Lesly Juarez <leslyj2@gmail.com>
2020-04-20 20:05:07 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,9 +8,8 @@ end
 # call your method here!
 
 ["Tim", "Tom", "Jim"]
-hello_t(["Tim","Tom", "Jim"]) do |name|
-  if name.start_with? ("T")
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
     puts "Hi, #{name}"
   end
 end
-

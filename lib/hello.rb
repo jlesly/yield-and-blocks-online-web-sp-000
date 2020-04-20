c8ce7c2562f23834f30edfb763b3fecf1db1b91d
Lesly Juarez <leslyj2@gmail.com>
2020-04-20 20:07:39 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,16 +1,28 @@
-def hello_t(array)
-  i = 0 
-  
-  while i < array.length yield(array[i]) i = i + 1 
-end
-array 
-end
+list = ["ab", "cd", "ef"]
 
-# call your method here!
-
-name= ["Tim", "Tom", "Jim"]
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+def my_each(array)
+  i = 0
+  while i < array.length
+# The "while" method will always return a value of => nil"
+    yield(array[i])
+    i = i + 1
   end
 end
+
+my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
+
+# Use the collection method 
+# To make the *return-value* the answer you want
+# instead of "=> nil"
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(list) {|i| "Hello #{i} what's up?"}
